88ecf28ab5356244b006341a6cf23ae213358594
Ethan <edavis@lclark.edu>
2022-02-07 21:43:27 +0000

    test 2.7

---
+++ test.r
@@ -0,0 +1,19 @@
+library("tidyverse")
+library("googledrive")
+drive_find(n_max = 30)
+
+
+system('git config --global user.email "edavis@lclark.edu"')
+library('usethis')
+
+usethis::git_sitrep()
+usethis::create_github_token()
+gitcreds::gitcreds_set()
+
+
+use_git()
+use_github()
+git_default_branch_rediscover()
+system('git branch -m master main')
+
+system('git commit -am "test 2.7"')
